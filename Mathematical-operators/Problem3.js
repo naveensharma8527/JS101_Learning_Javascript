632b43bfe6c5b6749f2858ec86e29c5bf7245c64
naveensharma8527 <nsnaveensharma1998@gmail.com>
2022-09-03 18:54:03 +0530

    Day 2

---
+++ Mathematical-operators/Problem3.js
@@ -0,0 +1,22 @@
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+
+
+
+// Updated Value
+
+one = one;
+two = two * 2;
+five = five * 2;
+six = six * 2;
+three = three * 3;
+four = four * 3;
+
+// Result
+console.log(one + two + three + four + five + six);
+
+
